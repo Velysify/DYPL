344673b8fa43b2f935209357d4ff5c87e2799dcf
velysify <velysify@gmail.com>
2015-05-20 10:39:53 +0200

    Woops

---
+++ testSpotipy.py
@@ -0,0 +1,19 @@
+import spotipy
+
+baseball_uri = 'spotify:artist:3Ek1EdkcbRFvSDJj9j5JXQ'
+spotify = spotipy.Spotify()
+
+results = spotify.artist_albums(baseball_uri, album_type='album')
+albums = results['items']
+while results['next']:
+	results = spotify.next(results)
+	albums.extend(results['items'])
+
+for album in albums:
+        trackresult = spotify.album_tracks(album['id'])
+        tracks = trackresult['items']
+        while trackresult['next']:
+                trackresult = spotify.next(trackresult)
+                tracks.extend(trackresult['items'])
+        for track in tracks:
+                print(track['name'])
