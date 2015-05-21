f0a92b9ae7718beaf8079baf1bf63f5bfe7e5dd2
HenrikCar <henrik.c.m.carlsson@gmail.com>
2015-05-21 15:09:02 +0200

    Merge remote-tracking branch 'remotes/origin/master'
    Conflicts:
    	PlaylistMergerClasses.py

--- MergingAlgorithms.py
+++ MergingAlgorithms.py
@@ -1,12 +1,22 @@
+import unittest
+import unittest.
 def mc_based_on_common_tastes(*matching_categories):
-
+    #Returns a matching_category containing only the songs present in all of the playlists
+    #Hopelessly ugly and ineffective, should be rewritten
     merged_matching_category = {}
     for mc in matching_categories:
         for entry in mc.keys():
             if entry in merged_matching_category:
                 merged_matching_category[entry][0] += mc[entry][0]
             else:
-                merged_matching_category[entry]
+                merged_matching_category[entry] = mc[entry]
+
+    for entry in merged_matching_category.keys():
+        if merged_matching_category[entry][0]<2:
+            del merged_matching_category[entry]
+
+    return merged_matching_category
+
 
 def mc_by_compromising(*matching_categories):
     pass
