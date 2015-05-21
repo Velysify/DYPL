ffffe9fa4410b37f037686eea51e3c8118c2b9b2
SaintedShot <henry-hicks1993@gmail.com>
2015-05-21 14:00:23 +0200

    Nu finns låtar i arrayen, alltså track objects

--- PlaylistMergerClasses.py
+++ PlaylistMergerClasses.py
@@ -12,18 +12,28 @@ class Playlist:
 
     def __init__(self):
 
-        token = 'BQC7BRu-M77fxGTnm9F3ptIMcU-uxKkAWT_il2MtuCiI54Q70H2RlLYYKBGlH_Fsk3JT5U7zHYnUxXtskBcmUTYwDh8ETRNkq9wU6fbFhJ5sKg_0ZswDdF0s1qmuaYgudfeFNqqjO0pP5TX_nhr032m9drZqnffv08BsL_O4AnJmgy-m7W0XD7PzYM_-nkYF2TNKgmwf7EUyZJZ7q6F2fs4eHR4FbazSKj-1yI2EFJOcd5OwbkOSiLjO6UQtj3lO3eicUh9DnFnDY5mSq-KWyVC-5XpGP0CCINFL3QnpXH2y'
-        username = "sanna_19"
+        self.array_of_songs_in_playlist = []
+        token = 'BQCkSiwGY8pUHjpZnp1uYSAgX-fJhIUa1U1aY_nGlC5jNTFvxw-aUwnDNkM9QtML6xIdEwLpLGMuvjsB_t0oaNLEokpUBksyjkhir0Ivg5N8VdV7QO-E3bR7Eph4tBrfr8_VNcO4_k7hqdCJSF1e5dp2xmEyzkMQipFyWSl-Gy_hf9DVcfoZFQHltXg-zkNMOBSLdniWntB_sw1amybjL0RhOy9x-FjYgNlzBVAvce3xTgEQBAYM443qJ35M3fzfPKys-BvpNEdDLx9RvTrbKcrm3cteN0D_QSvWEpY-8Fmy'
+
+        
+    
         if token:
             sp = spotipy.Spotify(auth=token)
             sp.trace = False
-            results = sp.user_playlist(username, '2VkvjS7dNkqRiPIkgziliq')
-            print results
+            playlist = sp.user_playlist('sanna_19', '6n3iKhvGFFW4j0GbwcvCBa')
+            songs = playlist['tracks']
+            song = songs['items']
+            for i in song:
+                i = i['track']
+                self.array_of_songs_in_playlist.append(i)
+            
         else:
             print "Can't get token for", username
         
-        self.array_of_songs_in_playlist = [results]
-        print self.array_of_songs_in_playlist
+       
+        for song in self.array_of_songs_in_playlist:
+            print song['name']
+        
         matching_categories = []
 
     def generate_playlist(self):
@@ -31,44 +41,15 @@ class Playlist:
 
 class MatchingCategory:
 
-    def __init__(self, array_of_songs_in_playlist):
+    def __init__(self, playlist):
+        self.playlist_data = self.analyze_playlist(playlist)
         self.harmony_rating = 0
-        self.playlist_data = {}
+
     def generate_playlist(self):
         pass
 
-    def analyze_playlist(array_of_songs_in_playlist):
+    def analyze_playlist(playlist):
         pass
-
-class MatchingCategorySong(MatchingCategory):
-
-    def __init__(self, playlist):
-        self.playlist_data = self.analyze_playlist(playlist)
-
-        MatchingCategory.__init__(self, playlist)
-
-    def analyze_playlist(self, array_of_songs_in_playlist):
-        #Creates the dictionary object to return
-        playlist_analysis = {}
-
-        for track in array_of_songs_in_playlist:
-            #Creates an identifier for each track based on song and artist name, intended to work as a key in the dictionary
-            #Using this identifier rather than the track ID means a song from an artist will always count as the same, even if it's taken from two different albums
-            artist_song_identifier = str(track.name())+" - "+str(track.artists()[0].name())
-
-            if not artist_song_identifier in self.playlist_data.keys():
-                self.playlist_data[artist_song_identifier][0] = track.id()
-                self.playlist_data[artist_song_identifier][1] += 1
-            else:
-                #is this even necessary???
-                self.playlist_data[artist_song_identifier][0] = track.id()
-                self.playlist_data[artist_song_identifier][1] = 1
-
-
-
-
-
-
 
 def merge_matching_categories(*matching_categories):
     merging_algorithm = MergingAlgorithms.mc_based_on_common_tastes(*matching_categories)
