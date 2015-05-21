cf1957e4cb66f1b98a69e21dae17254ab154ae38
HenrikCar <henrik.c.m.carlsson@gmail.com>
2015-05-21 11:37:43 +0200

    iafsjdf

--- PlaylistMergerClasses.py
+++ PlaylistMergerClasses.py
@@ -11,14 +11,24 @@ class Playlist:
 class MatchingCategory:
 
     def __init__(self, playlist):
-        self.playlist_data = self.analyze_playlist(playlist)
         self.harmony_rating = 0
-
+        self.playlist_data = {}
     def generate_playlist(self):
         pass
 
     def analyze_playlist(playlist):
         pass
+
+class MatchingCategorySong(MatchingCategory):
+
+    def __init__(self, playlist):
+        self.playlist_data = self.analyze_playlist(playlist)
+
+        MatchingCategory.__init__(self, playlist)
+
+    def analyze_playlist(playlist):
+
+
 
 def merge_matching_categories(*matching_categories):
     merging_algorithm = MergingAlgorithms.mc_based_on_common_tastes(*matching_categories)
