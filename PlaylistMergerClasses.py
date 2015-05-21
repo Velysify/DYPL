f0c2107ab78f85c06c12991f7ee7658dc660e293
SaintedShot <henry-hicks1993@gmail.com>
2015-05-21 11:33:23 +0200

    start i mergerclass

--- PlaylistMergerClasses.py
+++ PlaylistMergerClasses.py
@@ -3,8 +3,27 @@ import MergingAlgorithms
 import spotipy
 import spotipy.util as util
 
+spotify = spotipy.Spotify()
+
+
+
+
 class Playlist:
+
     def __init__(self):
+
+        token = 'BQC7BRu-M77fxGTnm9F3ptIMcU-uxKkAWT_il2MtuCiI54Q70H2RlLYYKBGlH_Fsk3JT5U7zHYnUxXtskBcmUTYwDh8ETRNkq9wU6fbFhJ5sKg_0ZswDdF0s1qmuaYgudfeFNqqjO0pP5TX_nhr032m9drZqnffv08BsL_O4AnJmgy-m7W0XD7PzYM_-nkYF2TNKgmwf7EUyZJZ7q6F2fs4eHR4FbazSKj-1yI2EFJOcd5OwbkOSiLjO6UQtj3lO3eicUh9DnFnDY5mSq-KWyVC-5XpGP0CCINFL3QnpXH2y'
+
+        if token:
+            sp = spotipy.Spotify(auth=token)
+            sp.trace = False
+            results = sp.user_playlist('sanna_19', '2VkvjS7dNkqRiPIkgziliq', tracks)
+            print results
+        else:
+            print "Can't get token for", username
+        
+        self.array_of_songs_in_playlist = [results]
+        print array_of_songs_in_playlist
         matching_categories = []
 
     def generate_playlist(self):
@@ -31,3 +50,5 @@ def merge_plalists(*playlists):
     merging_algorithm = MergingAlgorithms.pl_best(*playlists)
 
     return merging_algorithm(*playlists)
+
+test = Playlist()
